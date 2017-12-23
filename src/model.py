0ab75d305c6322894cc2c188e712299fce446fcb
Gideon Providence <g@math.utoronto.ca>
2017-12-23 14:38:19 -0500

    model updates

--- src/model.py
+++ src/model.py
@@ -1,6 +1,12 @@
 import torch
 from torch import nn, autograd, optim
+import torchvision as tv
 import matplotlib.pyplot as plt
+
+import data
+
+DEFAULT_CONTENT_LAYERS = [ 'conv_4' ]
+DEFAULT_STYLE_LAYERS = [ 'conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5' ]
 
 ## Additional Layer Types
 class GramMatrix(nn.Module):
@@ -49,3 +55,31 @@ class StyleLoss(nn.Module):
     def backward(self, retain_graph=True):
         self.loss.backward(retain_graph=retain_graph)
         return self.loss
+
+def load_vgg19():
+    vgg = tv.models.vgg19(pretrained=True).features
+    if data.HAS_CUDA:
+        vgg = vgg.cuda()
+    return vgg
+
+def build_model(style_img, content_img, style_weight=1000, content_weight=1, content_layers=DEFAULT_CONTENT_LAYERS, style_layers=DEFAULT_STYLE_LAYERS):
+    vgg = load_vgg19()
+    content_losses = []
+    style_losses = []
+    model = nn.Sequential()
+    gram = GramMatrix()
+
+    if data.HAS_CUDA:
+        model.cuda()
+        gram.cuda()
+
+    i = 1
+    for layer in list(vgg):
+        if isinstance(layer, nn.Conv2d):
+        elif isinstance(layer, nn.ReLU):
+        elif isinstance(layer, nn.MaxPool2d):
+                
+
+vgg = load_vgg19()
+for layer in list(vgg):
+    print(repr(layer))
